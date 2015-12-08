96d4a146be7219ffea731184c05b00126d3da295
garderony <y.garderon@gmail.com>
2015-12-08 22:08:44 +0100

    Changement de libratie i2c, plus légere

--- cmsis_lib/include/stm32f4xx_i2c.h
+++ cmsis_lib/include/stm32f4xx_i2c.h
@@ -1,24 +1,4 @@
-/**
-  ******************************************************************************
-  * @file    stm32f4xx_i2c.h
-  * @author  MCD Application Team
-  * @version V1.0.0
-  * @date    30-September-2011
-  * @brief   This file contains all the functions prototypes for the I2C firmware
-  *          library.
-  ******************************************************************************
-  * @attention
-  *
-  * THE PRESENT FIRMWARE WHICH IS FOR GUIDANCE ONLY AIMS AT PROVIDING CUSTOMERS
-  * WITH CODING INFORMATION REGARDING THEIR PRODUCTS IN ORDER FOR THEM TO SAVE
-  * TIME. AS A RESULT, STMICROELECTRONICS SHALL NOT BE HELD LIABLE FOR ANY
-  * DIRECT, INDIRECT OR CONSEQUENTIAL DAMAGES WITH RESPECT TO ANY CLAIMS ARISING
-  * FROM THE CONTENT OF SUCH FIRMWARE AND/OR THE USE MADE BY CUSTOMERS OF THE
-  * CODING INFORMATION CONTAINED HEREIN IN CONNECTION WITH THEIR PRODUCTS.
-  *
-  * <h2><center>&copy; COPYRIGHT 2011 STMicroelectronics</center></h2>
-  ******************************************************************************
-  */
+
 
 /* Define to prevent recursive inclusion -------------------------------------*/
 #ifndef __STM32F4xx_I2C_H
@@ -31,19 +11,8 @@
 /* Includes ------------------------------------------------------------------*/
 #include "stm32f4xx.h"
 
-/** @addtogroup STM32F4xx_StdPeriph_Driver
-  * @{
-  */
 
-/** @addtogroup I2C
-  * @{
-  */
-
-/* Exported types ------------------------------------------------------------*/
-
-/**
-  * @brief  I2C Init structure definition
-  */
+//brief  I2C Init structure definition
 
 typedef struct
 {
